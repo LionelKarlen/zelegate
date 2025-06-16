e3b9296e55e9a0f3ccebc6dc85c6f69f4ac06d4a
Lionel Karlen <lionel.karlen@protonmail.com>
2025-06-16 04:30:59 +0200

    build: add nix derivation

---
+++ default.nix
@@ -0,0 +1,29 @@
+{pkgs ? import <nixpkgs> {}}: let
+  external_binaries = with pkgs; [
+    fzf
+    fd
+  ];
+in
+  pkgs.stdenv.mkDerivation rec {
+    pname = "zelegate";
+    version = "0.1.0";
+
+    src = ./.;
+
+    nativeBuildInputs = with pkgs; [
+      nim
+      makeWrapper
+    ];
+
+    buildPhase = ''
+      export XDG_CACHE_HOME=$(mktemp -d)
+      nim c -d:release --out:zelegate ./zelegate.nim
+    '';
+
+    installPhase = ''
+      mkdir -p $out/bin
+      install -Dm755 zelegate $out/bin
+
+      wrapProgram $out/bin/zelegate --prefix PATH : "${pkgs.lib.makeBinPath external_binaries}"
+    '';
+  }
